f10d6807a877f75ed1006e374de5dc3cf80d5ce3
Dersy2000 <32402583+Dersy2000@users.noreply.github.com>
2018-01-29 20:32:14 -0800

    end of night 1/29

---
+++ app/src/main/java/mindcraft3495/scout/Teleop.java
@@ -0,0 +1,13 @@
+package mindcraft3495.scout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teleop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teleop);
+    }
+}
