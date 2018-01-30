1906d395b4cfce7ea9f3880d74a3c93c83f2d323
Dersy2000 <32402583+Dersy2000@users.noreply.github.com>
2018-01-29 20:22:50 -0800

    Teleop started

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -55,17 +55,17 @@ public final class R {
         public static final int adjust_height = 0x7f07001b;
         public static final int adjust_width = 0x7f07001c;
         public static final int auto = 0x7f070021;
-        public static final int dark = 0x7f070036;
-        public static final int icon_only = 0x7f070047;
-        public static final int light = 0x7f07004c;
-        public static final int none = 0x7f070057;
-        public static final int normal = 0x7f070058;
-        public static final int radio = 0x7f070061;
-        public static final int standard = 0x7f07007e;
-        public static final int text = 0x7f070083;
-        public static final int text2 = 0x7f070084;
-        public static final int wide = 0x7f070095;
-        public static final int wrap_content = 0x7f070098;
+        public static final int dark = 0x7f070043;
+        public static final int icon_only = 0x7f070054;
+        public static final int light = 0x7f070059;
+        public static final int none = 0x7f070064;
+        public static final int normal = 0x7f070065;
+        public static final int radio = 0x7f07006e;
+        public static final int standard = 0x7f07008b;
+        public static final int text = 0x7f070090;
+        public static final int text2 = 0x7f070091;
+        public static final int wide = 0x7f0700aa;
+        public static final int wrap_content = 0x7f0700ad;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f080004;
