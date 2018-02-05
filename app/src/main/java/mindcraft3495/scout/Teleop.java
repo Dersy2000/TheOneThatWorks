19638e4cc4364c99f3763a39516f8e9c1de10721
Dersy2000 <32402583+Dersy2000@users.noreply.github.com>
2018-02-05 11:29:56 -0800

    fixed buttons and getting auckland up to date

--- app/src/main/java/mindcraft3495/scout/Teleop.java
+++ app/src/main/java/mindcraft3495/scout/Teleop.java
@@ -3,11 +3,17 @@ package mindcraft3495.scout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Teleop extends AppCompatActivity {
+    private DatabaseReference dataBaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teleop);
+
+        dataBaseRef = FirebaseDatabase.getInstance().getReference();
     }
 }
