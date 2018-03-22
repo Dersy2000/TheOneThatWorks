e924d9b046399ddc662b7b782ccc2661e731e5d6
Dersy2000 <32402583+Dersy2000@users.noreply.github.com>
2018-03-21 22:41:04 -0700

    version sent to team

--- app/src/main/java/mindcraft3495/scout/Teleop.java
+++ app/src/main/java/mindcraft3495/scout/Teleop.java
@@ -177,7 +177,6 @@ public class Teleop extends AppCompatActivity {
         bClimbed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!climbed){
                     if(!climbed){
                         climbed = true;
                         bClimbed.setBackgroundColor(Color.GREEN);
@@ -186,7 +185,7 @@ public class Teleop extends AppCompatActivity {
                         bClimbed.setBackgroundColor(Color.RED);
                     }
                 }
-            }
+
         });
         bRobot.setOnClickListener(new View.OnClickListener() {
             @Override
